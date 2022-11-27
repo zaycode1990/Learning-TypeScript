642b9569266be249bcc4b4a592d73200249d4c69
Isaiah Jefferson <ijandicos90@gmail.com>
2022-11-27 05:14:30 -0500

    Working with multiple type args in TypeScript

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
-var number1 = 5;
-var number2 = 2.8;
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultStatement = 'Result is: ';
 
-var result = add(number1, number2);
+add(number1, number2, printResult, resultStatement);
 
-console.log(result);
+
