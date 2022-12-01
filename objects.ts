dc40fc1dc5f86253baea37f675aa55bbf734de27
Isaiah Jefferson <ijandicos90@gmail.com>
2022-11-30 21:34:32 -0500

    Learned how to set enums and arrays in TypeScript

--- objects.ts
+++ objects.ts
@@ -7,11 +7,33 @@ const person: {
     age: 30
 };
 
+enum Tier {TIER1, TIER2, TIER3 = 'Tier3'}; // enum variables, can set enum values such as third enum
+
+//typescript creation of array variable
+let favHobbies: string[];
+favHobbies = ['basketball']
 //optimal syntax when creating object
 const betterPerson = {
     name: "Isaiah Jefferson",
-    age: 32
+    age: 32,
+    hobbies: ['Coding', 'Exercising', 'Gaming'],
+    role: [2, 'software developer'] //tuple inferred
 }
+
+//setting a tuple in an object
+const newPerson: {
+name: string;
+age: number;
+hobbies: string[],
+role: [number, string]; 
+} = {
+name: "Isaiah Jefferson",
+age: 32,
+hobbies: ['Coding', 'Exercising', 'Gaming'],
+role: [2, 'software developer']
+}
+
+
     
 //alternative object creation
 
